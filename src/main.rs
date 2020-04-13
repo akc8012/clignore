2e8862ebce6a70670fc289d10bb6d9315a7c18e1
Andrew Colannino <akc8012@gmail.com>
2020-04-13 17:09:34 -0400

    include clap cli cratey boi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::App;
+
 fn main() {
-	println!("Hello, world!");
+	App::new("ignore-cli")
+		.version("0.1.0")
+		.about("Finds gitignore files")
+		.author("Andrew Colannino")
+		.get_matches();
+
+	println!("yahoo");
 }
