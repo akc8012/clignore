8de95bf901db40d13e39f6d55858ffe17eacc4f0
Andrew Colannino <akc8012@gmail.com>
2020-04-13 18:59:20 -0400

    notes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use ignore_cli::RequestMaker;
 fn main() {
 	App::new("ignore-cli")
 		.version("0.1.0")
-		.about("Finds gitignore files")
+		.about("Finds .gitignore files")
 		.author("Andrew Colannino")
 		.get_matches();
 
