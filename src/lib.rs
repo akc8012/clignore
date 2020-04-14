44615158abe6492ae92615ed1e22814e6cda2fe0
Andrew Colannino <akc8012@gmail.com>
2020-04-13 20:58:13 -0400

    working with reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use reqwest;
+use std::error::Error;
+
 #[derive(Default)]
 pub struct RequestMaker {}
 
@@ -6,7 +9,22 @@ impl RequestMaker {
 		RequestMaker {}
 	}
 
-	pub fn make_request(&self) {
-		println!("I am making a request now.");
+	pub fn make_request(&self) -> Result<String, Box<dyn Error>> {
+		let body = reqwest::blocking::get("https://www.rust-lang.org")?.text()?;
+		Ok(body)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn cheese() {
+		let request_maker = RequestMaker::new();
+		match request_maker.make_request() {
+			Ok(body) => println!("body = {:?}", body),
+			Err(error) => panic!("Problem making the request: {}", error)
+		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 use clap::App;
 
-use ignore_cli::RequestMaker;
-
 fn main() {
 	App::new("ignore_cli")
 		.version("0.1.0")
 		.about("Finds .gitignore files")
 		.author("Andrew Colannino")
 		.get_matches();
-
-	let request_maker = RequestMaker::new();
-	request_maker.make_request();
 }
