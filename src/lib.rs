8d7a4f0d122c254c6c9c1bbc9e5b9472a9c85547
Andrew Colannino <akc8012@gmail.com>
2020-04-14 20:34:31 -0400

    refactored thyself to remove self

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,29 @@
 use reqwest;
-use serde;
-use std::error::Error;
+use serde::de::DeserializeOwned;
+
+type ErrorBox = Box<dyn std::error::Error>;
 
 #[derive(Default)]
-pub struct RequestMaker {}
+pub struct RequestMaker;
 
 impl RequestMaker {
-	pub fn new() -> RequestMaker {
-		RequestMaker {}
-	}
-
-	pub fn get(&self, url: &str) -> Result<String, Box<dyn Error>> {
-		let body = self.get_response(url)?.text()?;
+	pub fn get(url: &str) -> Result<String, ErrorBox> {
+		let body = Self::get_response(url)?.text()?;
 		Ok(body)
 	}
 
 	// todo: wrapper object for json value?
-	pub fn get_json(&self, url: &str) -> Result<serde_json::Value, Box<dyn Error>> {
-		let json = self.get_response(url)?.json()?;
+	pub fn get_json(url: &str) -> Result<serde_json::Value, ErrorBox> {
+		let json = Self::get_response(url)?.json()?;
 		Ok(json)
 	}
 
-	pub fn get_json_deserialized<T: serde::de::DeserializeOwned>(
-		&self,
-		url: &str,
-	) -> Result<T, Box<dyn Error>> {
-		let object: T = self.get_response(url)?.json()?;
+	pub fn get_json_deserialized<T: DeserializeOwned>(url: &str) -> Result<T, ErrorBox> {
+		let object: T = Self::get_response(url)?.json()?;
 		Ok(object)
 	}
 
-	fn get_response(&self, url: &str) -> Result<reqwest::blocking::Response, Box<dyn Error>> {
+	fn get_response(url: &str) -> Result<reqwest::blocking::Response, ErrorBox> {
 		let response = reqwest::blocking::get(url)?;
 
 		let status = response.status();
@@ -59,23 +53,21 @@ mod tests {
 
 	#[test]
 	fn given_request_expect_response() {
-		let response = RequestMaker::new().get(TODO_URL).expect(ERROR_MESSAGE);
+		let response = RequestMaker::get(TODO_URL).expect(ERROR_MESSAGE);
 		assert!(response.contains("\"id\": 1"), "Should receive JSON");
 	}
 
 	#[test]
 	fn given_bad_request_expect_error() {
 		assert!(
-			RequestMaker::new()
-				.get("https://jsonplaceholder.typicode.com/dank-memes")
-				.is_err(),
+			RequestMaker::get("https://jsonplaceholder.typicode.com/dank-memes").is_err(),
 			"Should receive 404"
 		);
 	}
 
 	#[test]
 	fn get_json_request_value() {
-		let json = RequestMaker::new().get_json(TODO_URL).expect(ERROR_MESSAGE);
+		let json = RequestMaker::get_json(TODO_URL).expect(ERROR_MESSAGE);
 
 		assert_eq!(json["id"], 1);
 		assert_eq!(json["completed"], false);
@@ -83,9 +75,7 @@ mod tests {
 
 	#[test]
 	fn get_deserialized_json_request_value() {
-		let todo: TodoItem = RequestMaker::new()
-			.get_json_deserialized(TODO_URL)
-			.expect(ERROR_MESSAGE);
+		let todo: TodoItem = RequestMaker::get_json_deserialized(TODO_URL).expect(ERROR_MESSAGE);
 
 		assert_eq!(todo.id, 1);
 		assert_eq!(todo.completed, false);
