b9ab25897c7cc5657559e66099977e64c132ecd2
Andrew Colannino <akc8012@gmail.com>
2020-04-15 20:14:15 -0400

    comment out annoying

--- src/github_request_maker.rs
+++ src/github_request_maker.rs
@@ -1,25 +1,25 @@
-use crate::request_maker::RequestMaker;
+// use crate::request_maker::RequestMaker;
 
-type ErrorBox = Box<dyn std::error::Error>;
+// type ErrorBox = Box<dyn std::error::Error>;
 
-#[derive(Default)]
-pub struct GitHubRequestMaker {
-	request_maker: RequestMaker,
-}
+// #[derive(Default)]
+// pub struct GitHubRequestMaker {
+// 	request_maker: RequestMaker,
+// }
 
-#[allow(dead_code)] // TODO: REMOVE WHEN CODE IS CALLED IN MAIN!!!!!!!!!
-impl GitHubRequestMaker {
-	pub fn new() -> GitHubRequestMaker {
-		GitHubRequestMaker {
-			request_maker: RequestMaker {},
-		}
-	}
+// #[allow(dead_code)] // TODO: REMOVE WHEN CODE IS CALLED IN MAIN!!!!!!!!!
+// impl GitHubRequestMaker {
+// 	pub fn new() -> GitHubRequestMaker {
+// 		GitHubRequestMaker {
+// 			request_maker: RequestMaker {},
+// 		}
+// 	}
 
-	pub fn make_github_request() -> Result<String, ErrorBox> {
-		// TODO: Fix 403 Forbidden
-		RequestMaker::get("https://api.github.com/rate_limit")
-	}
-}
+// 	pub fn make_github_request() -> Result<String, ErrorBox> {
+// 		// TODO: Fix 403 Forbidden
+// 		RequestMaker::get("https://api.github.com/rate_limit")
+// 	}
+// }
 
 // #[cfg(test)]
 // mod tests {
