b34af234918fcb68e7f972cadc2d8c5147323a52
Andrew Colannino <akc8012@gmail.com>
2020-04-15 20:20:35 -0400

    use an address to thyself

--- src/request_maker.rs
+++ src/request_maker.rs
@@ -11,25 +11,29 @@ pub struct RequestMaker;
 
 #[allow(dead_code)] // TODO: REMOVE WHEN CODE IS CALLED IN MAIN!!!!!!!!!
 impl RequestMaker {
-	pub fn get(url: &str) -> Result<String, ErrorBox> {
-		let body = Self::get_response(url)?.text()?;
+	pub fn new() -> RequestMaker {
+		RequestMaker {}
+	}
+
+	pub fn get(&self, url: &str) -> Result<String, ErrorBox> {
+		let body = self.get_response(url)?.text()?;
 		Ok(body)
 	}
 
 	// TODO: wrapper object for json value?
-	pub fn get_json(url: &str) -> Result<serde_json::Value, ErrorBox> {
-		let json = Self::get_response(url)?.json()?;
+	pub fn get_json(&self, url: &str) -> Result<serde_json::Value, ErrorBox> {
+		let json = self.get_response(url)?.json()?;
 		Ok(json)
 	}
 
 	// TODO: this can just call get_json
-	pub fn get_json_deserialized<T: DeserializeOwned>(url: &str) -> Result<T, ErrorBox> {
-		let object: T = Self::get_response(url)?.json()?;
+	pub fn get_json_deserialized<T: DeserializeOwned>(&self, url: &str) -> Result<T, ErrorBox> {
+		let object: T = self.get_response(url)?.json()?;
 		Ok(object)
 	}
 
-	fn get_response(url: &str) -> Result<blocking::Response, ErrorBox> {
-		let response = Self::create_client().get(url).send()?;
+	fn get_response(&self, url: &str) -> Result<blocking::Response, ErrorBox> {
+		let response = self.create_client().get(url).send()?;
 
 		let status = response.status();
 		if status != 200 {
@@ -40,7 +44,7 @@ impl RequestMaker {
 		Ok(response)
 	}
 
-	fn create_client() -> blocking::Client {
+	fn create_client(&self) -> blocking::Client {
 		let user_agent: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 		let mut token = String::new();
@@ -81,19 +85,19 @@ mod tests {
 
 	#[test]
 	fn given_request_expect_response() {
-		let response = RequestMaker::get(TODO_URL).expect(ERROR_MESSAGE);
+		let response = RequestMaker::new().get(TODO_URL).expect(ERROR_MESSAGE);
 		assert!(response.contains("\"id\": 1"), "Should receive JSON");
 	}
 
 	#[test]
 	fn given_bad_request_expect_error() {
 		let url = "https://jsonplaceholder.typicode.com/dank-memes";
-		assert!(RequestMaker::get(url).is_err(), "Should receive 404");
+		assert!(RequestMaker::new().get(url).is_err(), "Should receive 404");
 	}
 
 	#[test]
 	fn get_json_request_value() {
-		let json = RequestMaker::get_json(TODO_URL).expect(ERROR_MESSAGE);
+		let json = RequestMaker::new().get_json(TODO_URL).expect(ERROR_MESSAGE);
 
 		assert_eq!(json["id"], 1);
 		assert_eq!(json["completed"], false);
@@ -101,7 +105,9 @@ mod tests {
 
 	#[test]
 	fn get_deserialized_json_request_value() {
-		let todo: TodoItem = RequestMaker::get_json_deserialized(TODO_URL).expect(ERROR_MESSAGE);
+		let todo: TodoItem = RequestMaker::new()
+			.get_json_deserialized(TODO_URL)
+			.expect(ERROR_MESSAGE);
 
 		assert_eq!(todo.id, 1);
 		assert_eq!(todo.completed, false);
@@ -111,7 +117,7 @@ mod tests {
 	fn get_request_that_requires_user_agent() {
 		let url = "https://api.github.com/rate_limit"; // rate_limit doesn't incur API hit
 
-		let response = RequestMaker::get(&url).expect(ERROR_MESSAGE);
+		let response = RequestMaker::new().get(&url).expect(ERROR_MESSAGE);
 		assert!(response.contains("\"limit\":"), "Should receive JSON");
 	}
 
@@ -119,7 +125,7 @@ mod tests {
 	fn get_request_with_custom_header() {
 		let url = "https://api.github.com/rate_limit";
 
-		let json = RequestMaker::get_json(&url).expect(ERROR_MESSAGE);
+		let json = RequestMaker::new().get_json(&url).expect(ERROR_MESSAGE);
 		assert_eq!(json["resources"]["core"]["limit"], 5000); // rate_limit at 5000 when using auth header
 	}
 }
