a1c28f30fa40dcd1ffca07876e1ef175ee259234
Andrew Colannino <akc8012@gmail.com>
2020-04-17 21:33:51 -0400

    if list then list

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use clap::App;
+use auth_token::AuthToken;
+use clap::{App, Arg};
+use github_request_maker::GitHubRequestMaker;
+use request_maker::RequestMaker;
 
 mod auth_token;
 mod github_request_maker;
@@ -7,9 +10,32 @@ mod requester;
 mod test_request_maker;
 
 fn main() {
-	App::new("ignore_cli")
+	let matches = App::new("ignore_cli")
 		.version("0.1.0")
-		.about("Finds .gitignore files")
+		.about("Finds .gitignore template files")
 		.author("Andrew Colannino")
+		.arg(
+			Arg::with_name("list")
+				.short("l")
+				.long("list")
+				.help("List all gitignore template files"),
+		)
 		.get_matches();
+
+	if matches.is_present("list") {
+		list_files();
+	}
+}
+
+fn list_files() {
+	let token = AuthToken::new("token.txt");
+	let requester = RequestMaker::new(Some(token));
+	let request_maker = GitHubRequestMaker::new(requester);
+
+	println!(
+		"{:?}",
+		request_maker
+			.get_file_names(&request_maker.get_tree_id().unwrap())
+			.unwrap()
+	);
 }
