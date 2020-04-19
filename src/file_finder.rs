416c3f008f7f381a24fb31d6a573064bf177a688
Andrew Colannino <akc8012@gmail.com>
2020-04-18 22:33:02 -0400

    find insensitive

--- src/file_finder.rs
+++ src/file_finder.rs
@@ -5,7 +5,7 @@ impl FileFinder {
 	pub fn find(file_names: Vec<&str>, query: &str) -> Vec<String> {
 		let mut matches = Vec::new();
 		for file_name in file_names {
-			if file_name.contains(query) {
+			if file_name.to_lowercase().contains(&query.to_lowercase()) {
 				matches.push(String::from(file_name));
 			}
 		}
@@ -21,6 +21,13 @@ mod tests {
 	fn can_find_file() {
 		let file_names = vec!["yeet.gitignore", "yoink.gitignore", "quite.gitignore"];
 		let result = &FileFinder::find(file_names, "yeet")[0];
+		assert_eq!(result, "yeet.gitignore");
+	}
+
+	#[test]
+	fn can_find_file_case_insensitive() {
+		let file_names = vec!["yeet.gitignore", "yoink.gitignore", "quite.gitignore"];
+		let result = &FileFinder::find(file_names, "YeEt")[0];
 		assert_eq!(result, "yeet.gitignore");
 	}
 
