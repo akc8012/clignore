88479541a18b20ce3662c81fd05fb74f6e36c70f
Andrew Colannino <akc8012@gmail.com>
2020-04-21 00:09:08 -0400

    select an option boiiiiiiiiiii

--- src/option_presenter.rs
+++ src/option_presenter.rs
@@ -13,12 +13,19 @@ impl<'o> OptionPresenter<'o> {
 
 		for (element, option) in self.options.iter().enumerate() {
 			list.push_str(&format!("[{}] {}", element + 1, option));
-
 			if element < self.options.len() - 1 {
 				list.push('\n');
 			}
 		}
 		list
+	}
+
+	pub fn select_option(&self, option: usize) -> Option<&str> {
+		if option == 0 {
+			return None;
+		}
+
+		Some(&self.options[option - 1])
 	}
 }
 
@@ -35,6 +42,21 @@ mod tests {
 		assert_eq!(list, "[1] jank.meme\n[2] funky.time");
 	}
 
-	// #[test]
-	// fn can_select_option() {}
+	#[test]
+	fn can_select_option() {
+		let options = vec![String::from("jank.meme"), String::from("funky.time")];
+		let presenter = OptionPresenter::new(&options);
+
+		let option = presenter.select_option(2).unwrap();
+		assert_eq!(option, "funky.time");
+	}
+
+	#[test]
+	fn can_select_none_option() {
+		let options = vec![String::from("jank.meme"), String::from("funky.time")];
+		let presenter = OptionPresenter::new(&options);
+
+		let option = presenter.select_option(0);
+		assert_eq!(option, None);
+	}
 }
