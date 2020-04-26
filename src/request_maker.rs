868b8b5d975f07b99785dfa95cc9ff0c42fda269
Andrew Colannino <akc8012@gmail.com>
2020-04-25 22:50:29 -0400

    remove unneeded print

--- src/request_maker.rs
+++ src/request_maker.rs
@@ -56,8 +56,6 @@ impl RequestMaker {
 		let mut headers = header::HeaderMap::new();
 
 		if let Some(token) = &self.token {
-			println!("{}", &token.to_string());
-
 			headers.insert(
 				header::AUTHORIZATION,
 				header::HeaderValue::from_str(&token.to_string()).expect(
